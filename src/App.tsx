5dd2b4504c91edd54e506afc24b8de3e07989850
Rahul Anand <anandrahul596@gmail.com>
2024-05-29 19:51:32 +0530

    Created a Dynamically Rendered Form

---
+++ src/App.tsx
@@ -0,0 +1,187 @@
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import data from "./assets/data.json";
+
+type FormDataType = {
+  formId: string;
+  Survey: Survey[];
+};
+
+type Survey = {
+  id: number;
+  Question: string;
+  Type: string;
+  required: boolean;
+  minChar?: number;
+  pageNo: number;
+  minVal?: number;
+};
+
+const fetchJSON = (): FormDataType => {
+  //console.log(data);
+
+  const surveyItems: Survey[] = data.Survey.map((row: any) => ({
+    id: row.id,
+    Question: row.Question,
+    Type: row.Type,
+    required: row.required === "true",
+    minChar: row.minChar,
+    pageNo: row.pageNo,
+    minVal: row.minVal,
+  }));
+
+  //console.log(surveyItems);
+
+  const formData: FormDataType = {
+    formId: "asdfdvfd1234564",
+    Survey: surveyItems,
+  };
+
+  return formData;
+};
+
+const App = () => {
+  const [form, setForm] = useState<FormDataType>();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchJSON();
+      setForm(data);
+      //console.log(data);
+    };
+    loadData();
+  }, []);
+
+  const buildSchema = (surveyItems: Survey[]) => {
+    const schemaObject: any = {};
+
+    surveyItems.forEach((item) => {
+      let fieldSchema: any;
+      switch (item.Type) {
+        case "string":
+          fieldSchema = z.string();
+          if (item.minChar) fieldSchema = fieldSchema.min(item.minChar);
+          break;
+        case "number":
+          fieldSchema = z.number();
+          if (item.minVal) fieldSchema = fieldSchema.min(item.minVal);
+          break;
+        case "boolean":
+          fieldSchema = z.boolean();
+          break;
+        default:
+          throw new Error("Not of correct Type");
+      }
+      if (item.required) fieldSchema = fieldSchema.nonempty();
+      schemaObject[item.Question] = fieldSchema;
+    });
+    //console.log(schemaObject);
+    return z.object(schemaObject);
+  };
+
+  // const testSchema: ZodType<FormData> = z
+  //   .object({
+  //     firstName: z.string().min(2).max(30),
+  //     lastName: z.string().min(2).max(30),
+  //     email: z.string().email(),
+  //     age: z.number().min(18).max(70),
+  //     password: z.string().min(8).max(20),
+  //     confirmPassword: z.string().min(8).max(20),
+  //   })
+  //   .refine((data) => data.password === data.confirmPassword, {
+  //     message: "Passwords do not match",
+  //     path: ["confirmPassword"],
+  //   });
+
+  const schema = form ? buildSchema(form.Survey) : z.object({});
+  //console.log(schema);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const submitData = (data: any) => {
+    console.log("worked:", data);
+  };
+  const onError = (errors: any) => {
+    console.error("Validation Errors:", errors);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  if (!form) return <div>Loading...</div>;
+  return (
+    // <div>
+    //   <form onSubmit={handleSubmit(submitData, onError)}>
+    //     {form.Survey.map((item) => (
+    //       <div key={item.id}>
+    //         <label>{item.Question}:</label>
+    //         {item.Type === "string" && (
+    //           <input type="text" {...register(item.Question)} />
+    //         )}
+    //         {item.Type === "number" && (
+    //           <input
+    //             type="number"
+    //             {...register(item.Question, { valueAsNumber: true })}
+    //           />
+    //         )}
+    //         {item.Type === "boolean" && (
+    //           <input type="checkbox" {...register(item.Question)} />
+    //         )}
+    //       </div>
+    //     ))}
+    //     <input type="submit" />
+    //   </form>
+    // </div>
+    <form onSubmit={handleSubmit(submitData, onError)}>
+      {form.Survey.map(
+        (item) =>
+          item.pageNo === currentPage && (
+            <div key={item.id}>
+              <label>{item.Question}:</label>
+              {item.Type === "string" && (
+                <input type="text" {...register(item.Question)} />
+              )}
+              {item.Type === "number" && (
+                <input
+                  type="number"
+                  {...register(item.Question, { valueAsNumber: true })}
+                />
+              )}
+              {item.Type === "boolean" && (
+                <input type="checkbox" {...register(item.Question)} />
+              )}
+            </div>
+          )
+      )}
+      <div>
+        {currentPage > 1 && (
+          <button type="button" onClick={handlePreviousPage}>
+            Previous
+          </button>
+        )}
+        {currentPage < Math.max(...form.Survey.map((item) => item.pageNo)) && (
+          <button type="button" onClick={handleNextPage}>
+            Next
+          </button>
+        )}
+      </div>
+      {currentPage === Math.max(...form.Survey.map((item) => item.pageNo)) && (
+        <input type="submit" />
+      )}
+    </form>
+  );
+};
+
+export default App;
